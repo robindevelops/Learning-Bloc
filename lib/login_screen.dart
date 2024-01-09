d3772aac65a45a152770ee3d96883c262f7148d7
robindevelops <andleebshahid515@gmail.com>
2024-01-09 16:55:42 +0500

    Login Page is made

--- lib/login_screen.dart
+++ lib/login_screen.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: prefer_const_constructors
+
 import 'package:flutter/material.dart';
 
 class LoginPage extends StatefulWidget {
@@ -10,6 +12,34 @@ class LoginPage extends StatefulWidget {
 class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
-    return const Scaffold();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 28.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              "Login Now",
+              style: TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                  color: Colors.blue),
+            ),
+            SizedBox(height: 10),
+            TextField(
+              decoration: InputDecoration(hintText: "Email"),
+            ),
+            TextField(
+              decoration: InputDecoration(hintText: "Password"),
+            ),
+            SizedBox(height: 20),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("SignUp"),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
