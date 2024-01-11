4e55fc5502562a26313a57a6256a1f81c98babcb
robindevelops <andleebshahid515@gmail.com>
2024-01-11 12:10:34 +0500

    .

--- lib/BlocFolder/login_screen.dart
+++ lib/BlocFolder/login_screen.dart
@@ -1,7 +1,7 @@
 // ignore_for_file: prefer_const_constructors
 
 import 'package:flutter/material.dart';
-import 'package:flutter_application_1/bloc/auth_bloc.dart';
+import 'package:flutter_application_1/BlocFolder/bloc/auth_bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
 class LoginPage extends StatefulWidget {
